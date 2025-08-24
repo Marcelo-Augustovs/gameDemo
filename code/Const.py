21aefccd2f5cf8f3fccf6745cac17c693b5321d7
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2025-08-24 13:38:22 -0300

    Adicionado imagens, musicas e constantes gerais

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_ORANGE = (255,128,0)
+COLOR_WHITE = (128, 128, 128)
+
+# M 
+MENU_OPTION = ('NEW GAME',
+               'CONTINUE',
+               'SCORE',
+                'EXIT'
+)
+
+# W
+WIN_WIDTH = 800
+WIN_HEIGHT = 620
