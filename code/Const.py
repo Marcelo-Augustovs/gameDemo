b30804fa568e87a1baa717cbe310d6d8b9dd8d8f
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2025-08-27 14:23:46 -0300

    Ajustes e novas constantes adicionadas

--- code/Const.py
+++ code/Const.py
@@ -1,16 +1,22 @@
 # C
+import pygame
+
+
 COLOR_ORANGE = (255,128,0)
 COLOR_WHITE = (128, 128, 128)
 COLOR_YELLOW = (255, 215, 0)
 
 #E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'menu_inicial0': 0,
     'menu_inicial1': 1.9,
     'menu_inicial2': 1,
     'menu_inicial3': 1.9,
     'menu_inicial4': 2.5,
-    'Player': 1
+    'Player': 1,
+    'Enemy1': 1,
+    'Enemy2': 1
 }
 # M 
 MENU_OPTION = ('NEW GAME',
