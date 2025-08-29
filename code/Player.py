968e93738dce011d0e469611368919ea40f07541
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2025-08-29 14:55:20 -0300

    adicionado e implementado player death animacao. criando dash

--- code/Player.py
+++ code/Player.py
@@ -11,6 +11,8 @@ class Player(Entity):
         self.surf_walk = pygame.image.load("assets/images/player_walk.png").convert_alpha()
         self.surf_walk_atk = pygame.image.load("assets/images/player_walk_attack.png").convert_alpha()
         self.surf_hurt = pygame.image.load("assets/images/player_hurt.png").convert_alpha()
+        self.surf_dash = pygame.image.load("assets/images/player_dash.png").convert_alpha()
+        self.surf_death = pygame.image.load("assets/images/player_death.png")
 
         # Config idle
         self.directions = 4
@@ -24,6 +26,11 @@ class Player(Entity):
         self.frame_width_walk = self.surf_walk.get_width() // self.frames_per_row_walk
         self.frame_height_walk = self.surf_walk.get_height() // self.directions
 
+        # Config dash
+        self.frames_per_row_dash = 8
+        self.frame_width_dash = self.surf_dash.get_width() // self.frames_per_row_dash
+        self.frame_height_dash = self.surf_dash.get_height() // self.directions
+
         # Config walk atk
         self.frames_per_row_walk_atk = 6
         self.frame_width_walk_atk = self.surf_walk_atk.get_width() // self.frames_per_row_walk_atk
@@ -33,6 +40,11 @@ class Player(Entity):
         self.frames_per_row_hurt = 5
         self.frame_width_hurt = self.surf_hurt.get_width() // self.frames_per_row_hurt
         self.frame_height_hurt = self.surf_hurt.get_height() // self.directions
+        
+        #config death
+        self.frames_per_row_death = 7
+        self.frame_width_death = self.surf_death.get_width() // self.frames_per_row_death
+        self.frame_height_death = self.surf_death.get_height() // self.directions
 
         # Estado inicial
         self.state = "idle"  # idle, walk, attack
@@ -51,6 +63,9 @@ class Player(Entity):
         self.animation_speed = 5
 
     def move(self):
+        if self.state == "death":
+            return
+        
         pressed_key = pygame.key.get_pressed()
         self.moving = False
 
@@ -93,6 +108,9 @@ class Player(Entity):
 
 
     def attack(self):
+        if self.state == "death":
+            return
+        
         pressed_key = pygame.key.get_pressed()
         if self.state != "attack":  # só começa se não estiver atacando
             if pressed_key[pygame.K_j] or pressed_key[pygame.K_z]:
@@ -101,17 +119,48 @@ class Player(Entity):
                 self.animation_counter = 0
     
     def take_damage(self, damage):
-        """Aplica dano e inicia animação de 'hurt'"""
+        """Aplica dano e inicia animação de 'hurt' ou 'death'"""
         self.health -= damage
-        if self.health < 0:
+        if self.health <= 0:
             self.health = 0
-        self.state = "hurt"
-        self.current_frame = 0
-        self.animation_counter = 0
-        self.hurt_counter = 0    
+            self.state = "death"
+            self.current_frame = 0
+            self.animation_counter = 0
+            self.death_time = pygame.time.get_ticks()  # marca tempo da morte
+        else:
+            self.state = "hurt"
+            self.current_frame = 0
+            self.animation_counter = 0
+            self.hurt_counter = 0
+ 
 
     def get_frame(self):
-        if self.state == "hurt":
+        
+        if self.state == "death":
+            frame_width = self.frame_width_death
+            frame_height = self.frame_height_death
+            surf = self.surf_death
+            max_frames = self.frames_per_row_death   
+            speed = self.animation_speed
+
+            if self.current_frame < max_frames - 1:  # avança até o último frame
+                self.animation_counter += 1
+                if self.animation_counter >= speed:
+                    self.current_frame += 1
+                    self.animation_counter = 0
+            else:
+                self.current_frame = max_frames - 1  # trava no último
+
+            
+            return surf.subsurface(
+                (self.current_frame * frame_width,
+                self.row * frame_height,
+                frame_width, frame_height)
+            )
+
+        
+
+        elif self.state == "hurt":
             frame_width = self.frame_width_hurt
             frame_height = self.frame_height_hurt
             surf = self.surf_hurt
